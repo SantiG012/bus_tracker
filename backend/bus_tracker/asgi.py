6ecbe09d00c7e51bb27b9c4724cd8504cd3cca46
jeffersonamado <jeffersonamado@gmail.com>
2024-09-28 12:10:06 -0500

    chore(entrypoint): change production to develop server

--- backend/bus_tracker/asgi.py
+++ backend/bus_tracker/asgi.py
@@ -3,8 +3,11 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from apps.bus import routing
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bus_tracker.settings')
+django.setup()
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- backend/bus_tracker/settings.py
+++ backend/bus_tracker/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'h3fa4v51=#tq#rt*u3yi@lc*i@y^ibzuxza$fkz$g8@yxsvduj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1", "0.0.0.0"]
 
 # Application definition
 
