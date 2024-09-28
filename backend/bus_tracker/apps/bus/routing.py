7f8ace81972656c3f8566cd9e0fba99589a844e3
Santiago Gómez <@>
2024-09-28 10:48:41 -0500

    Relocate files

--- backend/bus_tracker/apps/bus/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .consumer import BusLocationConsumer
-
-websocket_urlpatterns = [
-    path('ws/<str:bus_id>/position', BusLocationConsumer.as_asgi())
-]
--- backend/bus_tracker/apps/route/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.urlpatterns import format_suffix_patterns
-from django.urls import re_path
-from .views import *
-
-urlpatterns = [
-    re_path(r'$', ListCreateRoutes.as_view(), name='list-create-routes'),
-    re_path(r'^(?P<id>\d+)/$', RetrieveUpdateDeleteRoute.as_view(), name='get-update-delete-route')
-]
