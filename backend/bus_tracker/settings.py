8f2bae6f79244db1225e2ff4831a391c9a908d42
Santiago Gómez <@>
2024-10-05 10:26:20 -0500

    Add CORS

--- backend/bus_tracker/settings.py
+++ backend/bus_tracker/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [ '*', 'localhost', '127.0.0.1', '0.0.0.0']
 
 INSTALLED_APPS = [
     'daphne',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,6 +50,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,6 +62,17 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'bus_tracker.urls'
+
+
+
+CORS_ORIGIN_ALLOW_ALL = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
 
 TEMPLATES = [
     {
