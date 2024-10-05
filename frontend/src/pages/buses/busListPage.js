f168db87c8586dd3ed19bac8c9dafebe54551d8e
jeffersonamado <jeffersonamado@gmail.com>
2024-10-05 09:58:54 -0500

    fix(bus): improve bus form

--- frontend/src/pages/buses/busListPage.js
+++ frontend/src/pages/buses/busListPage.js
@@ -23,42 +23,46 @@ const BusListPage = () =>{
   }, []);
 
   const createOrUpdate = async (a_bus) =>{
-  if(isEditing){
-    await updateBus(selected.id, a_bus); 
-    setBus(empty)
-  }else{
-    await createBus(a_bus); 
-    setBus(empty)
-  }
+    if(isEditing){
+      await updateBus(selected.id, a_bus); 
+      setBus(empty)
+      const data = await fetchBuses();
+      setBuses(data);
+    }else{
+      await createBus(a_bus); 
+      setBus(empty)
+      const data = await fetchBuses();
+      setBuses(data);
+    }
   }
 
   const handleEdit = (a_bus) =>{
-  setBus(a_bus)
-  setIsEditing(true)
+    setBus(a_bus)
+    setIsEditing(true)
   }
 
   const handleDelete  = async (a_bus) =>{
-  await deleteBus(a_bus.id); 
-  const data = await fetchBuses();
-  setBuses(data);
+    await deleteBus(a_bus.id); 
+    const data = await fetchBuses();
+    setBuses(data);
   }
 
   return (
-  <div>
-    {/* Formulario */}
-    <BusForm busData={selected} onSubmit={createOrUpdate}/>
-    {/* lista de buses */}
-    <div className="bus-list">
-      {buses.map((a_bus)=>(
-        <BusCard 
-          key={a_bus.id}
-          a_bus={a_bus}
-          onEdit={handleEdit}
-          onDelete={handleDelete}
-          />
-        ))}
+    <div>
+      {/* Formulario */}
+      <BusForm busData={selected} onSubmit={createOrUpdate}/>
+      {/* lista de buses */}
+      <div className="bus-list">
+        {buses.map((a_bus)=>(
+          <BusCard 
+            key={a_bus.id}
+            a_bus={a_bus}
+            onEdit={handleEdit}
+            onDelete={handleDelete}
+            />
+          ))}
+      </div>
     </div>
-  </div>
   )
 };
 
